24c12c212135820fde0b652b9668a9bea33f8273
hyemi1129 <kimhyemi1243@gmail.com>
2023-07-28 19:44:15 +0900

    1068

--- codeup.c
+++ codeup.c
@@ -44,3 +44,18 @@ int main()
     else
     printf("odd");
 } */
+
+/* #include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a>=90)
+    printf("A");
+    else if (a>=70)
+    printf("B");
+    else if (a>=40)
+    printf("C");
+    else 
+    printf("D");
+} */
